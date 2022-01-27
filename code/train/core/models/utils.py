a9b86169f8cd4cdaa4dfb72f9737673c86e98db7
eileenhsieh <hsieh.eileen@inventec.com>
2022-01-27 06:17:44 +0000

    feat: warm up scheduler

---
+++ code/train/core/models/utils.py
@@ -0,0 +1,21 @@
+from torch import optim
+import numpy as np
+
+class CosineWarmupScheduler(optim.lr_scheduler._LRScheduler):
+
+    def __init__(self, optimizer, warmup, max_iters):
+        self.warmup = warmup
+        self.max_num_iters = max_iters
+        super().__init__(optimizer)
+
+    def get_lr(self):
+        lr_factor = self.get_lr_factor(epoch=self.last_epoch)
+        return [base_lr * lr_factor for base_lr in self.base_lrs]
+
+    def get_lr_factor(self, epoch):
+        lr_factor = 0.5 * (1 + np.cos(np.pi * epoch / self.max_num_iters))
+        if epoch <= self.warmup:
+            lr_factor *= epoch * 1.0 / self.warmup
+        # lr_factor = self.warmup_steps ** 0.5 * min(epoch ** (-0.5), epoch * self.warmup_steps ** (-1.5))
+        
+        return lr_factor
