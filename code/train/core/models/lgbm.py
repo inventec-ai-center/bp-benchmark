b1223e33089640980c6cbcd609ba872ba1b75e94
eileenhsieh <hsieh.eileen@inventec.com>
2022-02-14 07:21:21 +0000

    chore: rm useless import

--- code/train/core/models/lgbm.py
+++ code/train/core/models/lgbm.py
@@ -1,19 +1,8 @@
-import os
-from sklearn.impute import SimpleImputer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVR
 from lightgbm import LGBMRegressor
-# from .base_pl import Regressor
-
-# def toy(kernel, C):
-#     regressor = SVR(kernel=kernel, C=C)
-#     return regressor
 
 class lgbModel:
     def __init__(self, param_model, random_state=100):
         super(lgbModel,self).__init__()
-        # self.model = toy(param_model.kernel, param_model.C)
-        
         
         self.model = LGBMRegressor(n_estimators=param_model['n_estimators'],
                           max_depth=param_model['max_depth'],
