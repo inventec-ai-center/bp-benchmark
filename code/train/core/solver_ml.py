90be4acaaa292d29cc5c2f87ab77ce1130677ca4
eileenhsieh <hsieh.eileen@inventec.com>
2022-02-15 08:27:57 +0000

    feat: add ml model, mlp multioutput

--- code/train/core/solver_ml.py
+++ code/train/core/solver_ml.py
@@ -23,10 +23,16 @@ logger = logging.getLogger(__name__)
 #%%
 class SolverML(Solver):
     def _get_model(self):
-        if self.config.exp.model_type == "lgbm":
+        if self.config.exp.model_type == "lgb":
             model = lgbModel(self.config.param_model, self.config.exp.random_state)
         elif self.config.exp.model_type == "rf":
-            model = lgbModel(self.config.param_model, random_state=self.config.exp.random_state)
+            model = rfModel(self.config.param_model, random_state=self.config.exp.random_state)
+        elif self.config.exp.model_type == "svr":
+            model = svrModel(self.config.param_model, random_state=self.config.exp.random_state)
+        elif self.config.exp.model_type == "mlp":
+            model = mlpModel(self.config.param_model, random_state=self.config.exp.random_state)
+        elif self.config.exp.model_type == "ada":
+            model = adaModel(self.config.param_model, random_state=self.config.exp.random_state)
         else:
             model = eval(self.config.exp.model_type)(self.config.param_model, random_state=self.config.exp.random_state)
         return model
@@ -42,39 +48,78 @@ class SolverML(Solver):
             loader = dm.test_dataloader()
 
         bp_denorm = loader.dataset.bp_denorm
-        x, y = loader.dataset.all_ppg, loader.dataset.all_label.reshape(-1)
-
-        # prediction
-        pred_bp = bp_denorm(model.evaluate(x), target)
-
-        # gorund truth
-        true_bp = bp_denorm(y, target)
-
-        # naive
-        naive_bp =  np.mean(dm.train_dataloader(is_print=False).dataset.all_label, axis=0)
-        naive_bp = bp_denorm(naive_bp, target)
+        x, y = loader.dataset.all_ppg, loader.dataset.all_label#.reshape(-1)
 
         # error
-        bp_err = pred_bp - true_bp
+        if target==['SP', 'DP']:
+            # mlp outputs SP and DP simultaneously
+            # prediction
+            pred_bp = model.evaluate(x)
+            pred_sbp = bp_denorm(pred_bp[:,0], 'SP')
+            pred_dbp = bp_denorm(pred_bp[:,1], 'DP')
 
-        metrics = cal_metric({target: bp_err}, mode=mode)
+            # gorund truth
+            true_sbp = bp_denorm(y[:,0], 'SP')
+            true_dbp = bp_denorm(y[:,1], 'DP')
+
+            # naive
+            naive_bp =  np.mean(dm.train_dataloader(is_print=False).dataset.all_label, axis=0)
+            naive_sbp = bp_denorm(naive_bp[0], 'SP')
+            naive_dbp = bp_denorm(naive_bp[1], 'DP')
         
-        fold_errors[f"{mode}_subject_id"].append(loader.dataset.subjects)
-        fold_errors[f"{mode}_record_id"].append(loader.dataset.records)
-        fold_errors[f"{mode}_{target}_pred"].append(pred_bp)
-        fold_errors[f"{mode}_{target}_label"].append(true_bp)
-        fold_errors[f"{mode}_{target}_naive"].append([naive_bp]*len(pred_bp))
+            sbp_err = pred_sbp - true_sbp
+            dbp_err = pred_dbp - true_dbp
+            metrics = cal_metric({'SP': sbp_err, 'DP': dbp_err}, mode=mode)
+            fold_errors[f"{mode}_subject_id"].append(loader.dataset.subjects)
+            fold_errors[f"{mode}_record_id"].append(loader.dataset.records)
+            fold_errors[f"{mode}_SP_pred"].append(pred_sbp)
+            fold_errors[f"{mode}_SP_label"].append(true_sbp)
+            fold_errors[f"{mode}_SP_naive"].append([naive_sbp]*len(pred_bp))
+            fold_errors[f"{mode}_DP_pred"].append(pred_dbp)
+            fold_errors[f"{mode}_DP_label"].append(true_dbp)
+            fold_errors[f"{mode}_DP_naive"].append([naive_dbp]*len(pred_bp))
+        
+        else:
+            # prediction
+            pred_bp = bp_denorm(model.evaluate(x), target)
+
+            # gorund truth
+            true_bp = bp_denorm(y, target)
+
+            # naive
+            naive_bp =  np.mean(dm.train_dataloader(is_print=False).dataset.all_label, axis=0)
+            naive_bp = bp_denorm(naive_bp, target)
+
+            # error
+            bp_err = pred_bp - true_bp
+            metrics = cal_metric({target: bp_err}, mode=mode)
+            fold_errors[f"{mode}_subject_id"].append(loader.dataset.subjects)
+            fold_errors[f"{mode}_record_id"].append(loader.dataset.records)
+            fold_errors[f"{mode}_{target}_pred"].append(pred_bp)
+            fold_errors[f"{mode}_{target}_label"].append(true_bp)
+            fold_errors[f"{mode}_{target}_naive"].append([naive_bp]*len(pred_bp))
         
         return metrics
 
     def evaluate(self):
-        target = self.config.param_loader.label # sbp, dbp
-        fold_errors_template = {"subject_id":[],
-                                "record_id": [],
-                                f"{target}_naive":[],
-                                f"{target}_pred":[],
-                                f"{target}_label":[]
-                                }
+        target = self.config.param_loader.label # SP, DP
+        if target==['SP', 'DP']:
+            fold_errors_template = {"subject_id":[],
+                                    "record_id": [],
+                                    "SP_naive":[],
+                                    "SP_pred":[],
+                                    "SP_label":[],
+                                    "DP_naive":[],
+                                    "DP_pred":[],
+                                    "DP_label":[]
+                                    }
+        else:
+            fold_errors_template = {"subject_id":[],
+                                    "record_id": [],
+                                    f"{target}_naive":[],
+                                    f"{target}_pred":[],
+                                    f"{target}_label":[]
+                                    }
         
         fold_errors = {f"{mode}_{k}":[] for k,v in fold_errors_template.items() for mode in ["tr", "val","ts"]}
         
@@ -101,7 +146,7 @@ class SolverML(Solver):
             mf.pytorch.autolog()
             with mf.start_run(run_name=f"cv{foldIdx}", nested=True) as run:
                 x = dm.train_dataloader().dataset.all_ppg
-                y = dm.train_dataloader().dataset.all_label.reshape(-1)
+                y = dm.train_dataloader().dataset.all_label
 
                 # model = ToyModel(self.config.param_model)
                 model.fit(x,y)
@@ -120,13 +165,24 @@ class SolverML(Solver):
         # compute final metric
         out_metric = {}
         fold_errors = {k:np.concatenate(v, axis=0) for k,v in fold_errors.items() if len(v)!=0 }
-        bp_err = fold_errors[f"ts_{target}_naive"] - fold_errors[f"ts_{target}_label"] 
-        naive_metric = cal_metric({target:bp_err}, mode='nv')
-        out_metric.update(naive_metric)
-        for mode in ['tr', 'val', 'ts']:
-            bp_err = fold_errors[f"{mode}_{target}_pred"] - fold_errors[f"{mode}_{target}_label"] 
-            tmp_metric = cal_metric({target:bp_err}, mode=mode)
-            out_metric.update(tmp_metric)
+        if target==['SP', 'DP']:
+            sbp_err = fold_errors[f"ts_SP_naive"] - fold_errors[f"ts_SP_label"] 
+            dbp_err = fold_errors[f"ts_DP_naive"] - fold_errors[f"ts_DP_label"] 
+            naive_metric = cal_metric({'SP':sbp_err, 'DP':dbp_err}, mode='nv')
+            out_metric.update(naive_metric)
+            for mode in ['tr', 'val', 'ts']:
+                sbp_err = fold_errors[f"{mode}_SP_pred"] - fold_errors[f"{mode}_SP_label"] 
+                dbp_err = fold_errors[f"{mode}_DP_pred"] - fold_errors[f"{mode}_DP_label"] 
+                tmp_metric = cal_metric({'SP':sbp_err, 'DP':dbp_err}, mode=mode)
+                out_metric.update(tmp_metric)
+        else:  
+            bp_err = fold_errors[f"ts_{target}_naive"] - fold_errors[f"ts_{target}_label"] 
+            naive_metric = cal_metric({target:bp_err}, mode='nv')
+            out_metric.update(naive_metric)
+            for mode in ['tr', 'val', 'ts']:
+                bp_err = fold_errors[f"{mode}_{target}_pred"] - fold_errors[f"{mode}_{target}_label"] 
+                tmp_metric = cal_metric({target:bp_err}, mode=mode)
+                out_metric.update(tmp_metric)
 
         return out_metric
 
