54e2292f202e20f55a205db1cb635b91437a6352
eileenhsieh <hsieh.eileen@inventec.com>
2022-02-16 12:43:35 +0000

    refactor: simplify repeated if else

--- code/train/core/solver_ml.py
+++ code/train/core/solver_ml.py
@@ -3,13 +3,11 @@ import os
 import joblib
 import numpy as np 
 
-# Load loaders
-from core.loaders import *
-from core.utils import cal_metric
+# Load modules
 from core.solver import Solver
-
-# Load model
+from core.loaders import *
 from core.models import *
+from core.utils import cal_metric
 
 # Others
 import mlflow as mf
@@ -38,7 +36,7 @@ class SolverML(Solver):
         return model
         
     def get_cv_metrics(self, fold_errors, dm, model, mode="val"):
-        target = self.config.param_loader.label # sbp, dbp
+        target = self.config.param_loader.label # 'SP', 'DP'
 
         if mode=='tr':
             loader = dm.train_dataloader()
@@ -48,89 +46,58 @@ class SolverML(Solver):
             loader = dm.test_dataloader()
 
         bp_denorm = loader.dataset.bp_denorm
-        x, y = loader.dataset.all_ppg, loader.dataset.all_label#.reshape(-1)
+        x, y = loader.dataset.all_ppg, loader.dataset.all_label
 
-        # error
-        if target==['SP', 'DP']:
-            # mlp outputs SP and DP simultaneously
-            # prediction
-            pred_bp = model.evaluate(x)
-            pred_sbp = bp_denorm(pred_bp[:,0], 'SP')
-            pred_dbp = bp_denorm(pred_bp[:,1], 'DP')
-
-            # gorund truth
-            true_sbp = bp_denorm(y[:,0], 'SP')
-            true_dbp = bp_denorm(y[:,1], 'DP')
-
-            # naive
-            naive_bp =  np.mean(dm.train_dataloader(is_print=False).dataset.all_label, axis=0)
-            naive_sbp = bp_denorm(naive_bp[0], 'SP')
-            naive_dbp = bp_denorm(naive_bp[1], 'DP')
+        #--- Predict
+        m, n = y.shape[0], y.shape[1] if len(y.shape)==2 else 1
+        naive =  np.mean(dm.train_dataloader(is_print=False).dataset.all_label, axis=0)
+        naive = np.array([naive]) if isinstance(naive, np.float64) else naive
+        pred = model.evaluate(x).reshape(m, n)
+        y = y.reshape(m, n)
         
-            sbp_err = pred_sbp - true_sbp
-            dbp_err = pred_dbp - true_dbp
-            metrics = cal_metric({'SP': sbp_err, 'DP': dbp_err}, mode=mode)
-            fold_errors[f"{mode}_subject_id"].append(loader.dataset.subjects)
-            fold_errors[f"{mode}_record_id"].append(loader.dataset.records)
-            fold_errors[f"{mode}_SP_pred"].append(pred_sbp)
-            fold_errors[f"{mode}_SP_label"].append(true_sbp)
-            fold_errors[f"{mode}_SP_naive"].append([naive_sbp]*len(pred_bp))
-            fold_errors[f"{mode}_DP_pred"].append(pred_dbp)
-            fold_errors[f"{mode}_DP_label"].append(true_dbp)
-            fold_errors[f"{mode}_DP_naive"].append([naive_dbp]*len(pred_bp))
+        #--- Evaluate
+        # make target a list: ['SP'], ['DP'], ['SP', 'DP']
+        if target=='SP' or target=='DP':
+            target = [target]
         
-        else:
-            # prediction
-            pred_bp = bp_denorm(model.evaluate(x), target)
-
-            # gorund truth
-            true_bp = bp_denorm(y, target)
-
-            # naive
-            naive_bp =  np.mean(dm.train_dataloader(is_print=False).dataset.all_label, axis=0)
-            naive_bp = bp_denorm(naive_bp, target)
-
+        metrics = {}    
+        for i, tar in enumerate(target):
+            pred_bp = bp_denorm(pred[:, i], tar)
+            true_bp = bp_denorm(y[:, i], tar)
+            naive_bp = bp_denorm(naive[i], tar)
+        
             # error
             bp_err = pred_bp - true_bp
-            metrics = cal_metric({target: bp_err}, mode=mode)
+            metrics.update(cal_metric({tar: bp_err}, mode=mode))
             fold_errors[f"{mode}_subject_id"].append(loader.dataset.subjects)
             fold_errors[f"{mode}_record_id"].append(loader.dataset.records)
-            fold_errors[f"{mode}_{target}_pred"].append(pred_bp)
-            fold_errors[f"{mode}_{target}_label"].append(true_bp)
-            fold_errors[f"{mode}_{target}_naive"].append([naive_bp]*len(pred_bp))
-        
+            fold_errors[f"{mode}_{tar}_pred"].append(pred_bp)
+            fold_errors[f"{mode}_{tar}_label"].append(true_bp)
+            fold_errors[f"{mode}_{tar}_naive"].append([naive_bp]*len(pred_bp))
+            
         return metrics
 
     def evaluate(self):
         target = self.config.param_loader.label # SP, DP
-        if target==['SP', 'DP']:
-            fold_errors_template = {"subject_id":[],
-                                    "record_id": [],
-                                    "SP_naive":[],
-                                    "SP_pred":[],
-                                    "SP_label":[],
-                                    "DP_naive":[],
-                                    "DP_pred":[],
-                                    "DP_label":[]
-                                    }
-        else:
-            fold_errors_template = {"subject_id":[],
-                                    "record_id": [],
-                                    f"{target}_naive":[],
-                                    f"{target}_pred":[],
-                                    f"{target}_label":[]
-                                    }
+        # make target a list: ['SP'], ['DP'], ['SP', 'DP']
+        if target=='SP' or target=='DP':
+            target = [target]
+        
+        fold_errors_template = {"subject_id":[],
+                                "record_id": []}
+        for tar in target:
+            fold_errors_template[f"{tar}_naive"] = []
+            fold_errors_template[f"{tar}_pred"] = []
+            fold_errors_template[f"{tar}_label"] = []
+            
         
         fold_errors = {f"{mode}_{k}":[] for k,v in fold_errors_template.items() for mode in ["tr", "val","ts"]}
         
-        # =============================================================================
-        # data module
-        # =============================================================================
+        #--- data module
         dm = self._get_loader()
 
-        
         all_split_df = joblib.load(self.config.exp.subject_dict)
-        # Nested cv 
+        #--- Nested cv 
         for foldIdx in range(self.config.exp.N_fold):
             if (self.config.exp.cv=='HOO') and (foldIdx==1):  break
             train_df = all_split_df[foldIdx]['train']
@@ -139,16 +106,15 @@ class SolverML(Solver):
             
             dm.setup_kfold(train_df, val_df, test_df)
             
-            # Init model
+            #--- Init model
             model = self._get_model()
            
-            # trainer main loop
+            #--- trainer main loop
             mf.pytorch.autolog()
             with mf.start_run(run_name=f"cv{foldIdx}", nested=True) as run:
                 x = dm.train_dataloader().dataset.all_ppg
                 y = dm.train_dataloader().dataset.all_label
 
-                # model = ToyModel(self.config.param_model)
                 model.fit(x,y)
 
                 metrics = {}
@@ -158,7 +124,7 @@ class SolverML(Solver):
                 logger.info(f"\t {metrics}")
                 mf.log_metrics(metrics)
 
-            # Save to model directory
+            #--- Save to model directory
             os.makedirs(self.config.path.model_directory, exist_ok=True)
             
             
