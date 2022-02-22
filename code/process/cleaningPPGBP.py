fc5b2c94f38e7681d1e155fc342da161d52573cb
Sergiogvz <gonzalez-vazquez.sergio@inventec.com>
2022-02-22 16:37:32 +0800

    Fix the removal of a instance by index to by trial id

--- code/process/cleaningPPGBP.py
+++ code/process/cleaningPPGBP.py
@@ -222,6 +222,7 @@ def main(args):
 
 	#---------- Feature generation ----------#
 	_print_step("Segment removal by feature generation",cl_log)
+	
 	df = df.drop(index=df[df.trial=='203_3'].index[0]).reset_index(drop=True) ## Missed bad quality signal
 
 	data_feats, data_raw = _compute_features_df(df, args)
