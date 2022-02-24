f2caf6c3dfaeb7860478debf5345c75b128b9720
Sergiogvz <gonzalez-vazquez.sergio@inventec.com>
2022-02-24 15:26:46 +0800

    Improving bcg_mat2csv script to pass path of bcg raw data as parameter

--- code/process/core/bcg_mat2csv.m
+++ code/process/core/bcg_mat2csv.m
@@ -1,13 +1,25 @@
-% This script formats the data in mat files to csv files.
-% Thus, the data can be processed with python later on.
 
-mat = load('Bed_System_Database.mat');
-
-writetable(mat.Bed_System_Database(:,[1:5,7:8]), 'bcg_pat_info.csv');
-
-mat2 = load('Preprocessed_Database.mat');
-
-for c = 1:40
-    writetable(mat.Bed_System_Database.RawData{c,1}, strcat('pats/pat',num2str(c),'.csv'));
-    writetable(mat2.Preprocessed_Database.FilteredData{c,1}, strcat('pats/pat',num2str(c),'pred.csv'));
+function [] = bcg_mat2csv(path_raw)
+    % This script formats the data in mat files to csv files.
+    % Thus, the data can be processed with python later on.
+    if nargin > 0
+        path = path_raw;
+    else
+        path = '.';
+    end
+    
+    disp('Formating subjects info...')
+    mat = load(strcat(path,'/Bed_System_Database.mat'));
+    
+    writetable(mat.Bed_System_Database(:,[1:5,7:8]), strcat(path,'/bcg_pat_info.csv'));
+    
+    disp('Formating subjects signal...')
+    mat2 = load(strcat(path,'/Preprocessed_Database.mat'));
+    
+    mkdir(strcat(path,'pats/'))
+    for c = 1:40
+        disp(strcat('Sub',num2str(c)))
+        writetable(mat.Bed_System_Database.RawData{c,1}, strcat(path,'pats/pat',num2str(c),'.csv'));
+        writetable(mat2.Preprocessed_Database.FilteredData{c,1}, strcat(path,'pats/pat',num2str(c),'pred.csv'));
+    end
 end
